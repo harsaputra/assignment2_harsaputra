9c120bba97fef413ed7b0210517324e9d9e53817
harsaputra <69347333+harsaputra@users.noreply.github.com>
2024-11-13 15:28:17 +0700

    Add files via upload

---
+++ my_first_module.py
@@ -0,0 +1,5 @@
+def luas_persegi_panjang(panjang,lebar):
+  return panjang * lebar
+
+def keliling_persegi_panjang(panjang,lebar):
+  return panjang * 2 + lebar * 2
---
+++ rumus.py
@@ -0,0 +1,5 @@
+def luas_segitiga(alas,tinggi):
+  return alas * tinggi * 0.5
+
+def keliling_segitiga_sama_sisi(alas, sisi):
+  return alas + sisi + sisi
